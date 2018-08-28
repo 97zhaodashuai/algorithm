a8f74e095d1330cfa22d50edc062023fe259c311
zhaodsh <zhaodsh@fxiao.com>
2018-08-28 17:16:22 +0800

    commit qsort

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import graphs.bfs;
+import sort.quicksort;
 
 
 public class Main{
     public static void main(String[] args){
-        bfs.main(args);
+        //bfs.main(args);
+        quicksort.main(args);
     }
 
 }
---
+++ src/sort/quicksort.java
@@ -0,0 +1,18 @@
+package sort;
+
+public class quicksort {
+
+    public static void main(String[] args){
+        sort();
+    }
+
+    private static  void sort(){
+    
+
+    }
+
+
+
+
+
+}
